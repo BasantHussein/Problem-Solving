94e1b4d433e4fd8f1006e4d3b8ce5f052efd49d4
BasantHussein <basant_husseinn@yahoo.com>
2021-08-18 02:56:43 +0200

    added left rotation solution

---
+++ LeftRotation/Left_Rotation.py
@@ -0,0 +1,35 @@
+import os
+
+
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+def rotLeft(a, d):
+    # Write your code here
+    arr = []
+    for i in range(0, len(a)):
+        arr.insert(i, a[d % len(a)])
+        d += 1
+    return arr
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
